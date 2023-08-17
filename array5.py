a0f0c1aff3145800491ecee4004f4caf3c277eb4
ahaskauhsik <ahaskauhsik@gmail.com>
2023-08-17 12:00:48 +0530

    change array5

---
+++ array5.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array([[2,3,4],[5,6,7]])
+b = np.array([[9,7,6],[5,7,3]])
+print("Addition of 2 arrays= \n",a+b)
+print("\n")
+print("Substraction of 2 arrays= \n",a-b)
+print("\n")
+print("Multiplication of 2 arrays=\n",a*b)
+
