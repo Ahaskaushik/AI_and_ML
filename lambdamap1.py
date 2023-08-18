c2e99aacd377fb8d29c00a3976c7072ca71c832f
Ahaskaushik <129049250+Ahaskaushik@users.noreply.github.com>
2023-08-18 10:53:06 +0530

    Update lambdamap1.py

--- lambdamap1.py
+++ lambdamap1.py
@@ -7,4 +7,11 @@ lst=[1,2,3]
 sqrt=list(map(lambda x:x*x, lst))
 print(sqrt)
 print
+print("\n")
+
+lst1=[1,2,3]
+lst2=[4,5,6]
+lst3=[7,8,9]
+add=list(map(lambda x,y,z: x+y+z, lst1,lst2,lst3))
+print(add)
 
