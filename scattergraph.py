f44af1c5bec51ddfdc6ba2d9f277048a1a389a1e
ahaskauhsik <ahaskauhsik@gmail.com>
2023-08-23 14:56:07 +0530

    change

---
+++ scattergraph.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot  as plt
+
+data = pd.read_csv(r"C:\Users\SPTINT-04\Downloads\tips.csv")
+df=pd.DataFrame(data)
+print(df)
+plt.scatter(df['tip'],df['day'])
+plt.xlabel("days")
+plt.ylabel("tip")
+plt.show()
+
